ac16dfe791e6f5a905e454ed83e00f3acc8b66d8
Yash Kankal <yashamolkankal@outlook.com>
2026-03-19 15:34:18 -0700

    Initial Commit

---
+++ SpendLens/SpendLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpendLensApp.swift
+//  SpendLens
+//
+//  Created by Yashwardhan Kankal on 3/19/26.
+//
+
+import SwiftUI
+
+@main
+struct SpendLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
